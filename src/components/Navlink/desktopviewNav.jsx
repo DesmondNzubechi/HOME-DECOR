ae655de6ec1ff94035f9f5a99713a1fa3d3ce873
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-10 11:37:10 +0100

    REACT ROUTER NAVLINK ADDED

--- src/components/Navlink/desktopviewNav.jsx
+++ src/components/Navlink/desktopviewNav.jsx
@@ -1,22 +1,59 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import {AiFillShopping, AiFillHeart} from 'react-icons/ai';
 import {BsSearch} from 'react-icons/bs';
+import './style.css';
+
+
+
+
 export const DesktopNav = () => {
     return(
         <div className="lg:flex hidden flex-row py-[10px] z-[20]  fixed top-0 left-0 right-0 w-full px-[20px] items-center overflow-x-hidden bg-Tp  gap-5 justify-around">
             <div className="font-myfont"><h1 className="uppercase text-slate-100 font-bold text-[30px] ">HomeDecor</h1></div>
 
 
-<ul className="flex  font-fonty flex-row text-slate-200  gap-5 items-center ">
-<li><Link  to='/' className="text-[17px] " >Home</Link></li>
-<li><Link  to='/bedroom' className="text-[17px]">Bedroom</Link></li>
-<li><Link  to='/livingroom' className="text-[17px]">Living Room</Link></li>
-<li><Link  to='/dinningroom' className="text-[17px]">Dinning Room</Link></li>
-<li><Link  to='/office' className="text-[17px]">Office</Link></li>
-<li><Link  to='/outdoor' className="text-[17px]">Outdoor </Link></li>
-<li><Link  to='/decor' className="text-[17px]">Decor</Link></li>
-<li><Link  to='/kitchen' className="text-[17px]">Kitchen</Link></li>
+<ul id="links" className="flex  font-fonty flex-row text-slate-200  gap-5 items-center ">
+<li><NavLink  to='/'
+style={({isActive}) => {
+return isActive ? {borderBottom : '5px solid white'} : {}
+}}
+ className="text-[17px] " >Home</NavLink></li>
+<li><NavLink  
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+to='/bedroom' className="text-[17px]">Bedroom</NavLink></li>
+<li><NavLink  
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+to='/livingroom' className="text-[17px]">Living Room</NavLink></li>
+<li><NavLink  to='/dinningroom' 
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+className="text-[17px]">Dinning Room</NavLink></li>
+<li><NavLink  to='/office' 
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+className="text-[17px]">Office</NavLink></li>
+<li><NavLink  to='/outdoor'
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+ className="text-[17px]">Outdoor </NavLink></li>
+<li><NavLink  to='/decor'
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+ className="text-[17px]">Decor</NavLink></li>
+<li><NavLink  to='/kitchen'
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+ className="text-[17px]">Kitchen</NavLink></li>
 </ul>
 
 <div className="flex text-slate-100 flex-row gap-3  ">
--- src/components/Navlink/mobileviewNav.jsx
+++ src/components/Navlink/mobileviewNav.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import {AiFillShopping, AiFillHeart} from 'react-icons/ai';
 import {BsSearch} from 'react-icons/bs';
 import {FaBars} from 'react-icons/fa';
 import {HiOutlineXMark} from 'react-icons/hi2';
-
+import './style.css';
 
 export const MobileNav = () => {
 const [navIcons, SetNavIcons] = useState({
@@ -34,15 +34,47 @@ const HideSideLink = ()  => {
             <div className="font-myfont"><h1 className="uppercase text-slate-100 font-bold text-[20px] md:text-[30px] ">HomeDecor</h1></div>
 
 
-<ul className={`flex fixed transition ease-out duration-500 top-[50px] py-[40px] bg-black w-[50%] h-full ${sideLink}  font-fonty flex-col text-slate-200  gap-5 items-center`}>
-<li><Link onClick={HideSideLink} to='/' className="text-[17px] " >Home</Link></li>
-<li><Link onClick={HideSideLink} to='/bedroom' className="text-[17px]">Bedroom</Link></li>
-<li><Link onClick={HideSideLink} to='/livingroom' className="text-[17px]">Living Room</Link></li>
-<li><Link onClick={HideSideLink} to='/dinningroom' className="text-[17px]">Dinning Room</Link></li>
-<li><Link onClick={HideSideLink} to='/office' className="text-[17px]">Office</Link></li>
-<li><Link onClick={HideSideLink} to='/outdoor' className="text-[17px]">Outdoor </Link></li>
-<li><Link onClick={HideSideLink} to='/decor' className="text-[17px]">Decor</Link></li>
-<li><Link onClick={HideSideLink} to='/kitchen' className="text-[17px]">Kitchen</Link></li>
+<ul id="links" className={`flex fixed transition ease-out duration-500 top-[50px] py-[40px] bg-black w-[50%] h-full ${sideLink}  font-fonty flex-col text-slate-200  gap-5 items-center`}>
+<li><NavLink onClick={HideSideLink} to='/'
+style={({isActive}) => {
+return isActive ? {borderBottom : '5px solid white'} : {}
+}}
+ className="text-[17px] " >Home</NavLink></li>
+<li><NavLink onClick={HideSideLink} 
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+to='/bedroom' className="text-[17px]">Bedroom</NavLink></li>
+<li><NavLink onClick={HideSideLink} 
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+to='/livingroom' className="text-[17px]">Living Room</NavLink></li>
+<li><NavLink onClick={HideSideLink} to='/dinningroom' 
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+className="text-[17px]">Dinning Room</NavLink></li>
+<li><NavLink onClick={HideSideLink} to='/office' 
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+className="text-[17px]">Office</NavLink></li>
+<li><NavLink onClick={HideSideLink} to='/outdoor'
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+ className="text-[17px]">Outdoor </NavLink></li>
+<li><NavLink onClick={HideSideLink} to='/decor'
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+ className="text-[17px]">Decor</NavLink></li>
+<li><NavLink onClick={HideSideLink} to='/kitchen'
+style={({isActive}) => {
+    return isActive ? {borderBottom : '5px solid white'} : {}
+    }}
+ className="text-[17px]">Kitchen</NavLink></li>
 </ul>
 
 <div className="flex text-slate-100 items-center flex-row gap-3  ">
