d1ddf58429051fe2c8082a3c5c28fc22ab178478
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-19 18:11:04 +0100

    new change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -7,8 +7,6 @@ import { EmailCheckout } from "../../components/Checkout/CheckoutEmail";
 import { AddressCheckout } from "../../components/Checkout/CheckoutAddress";
 import { PaymentCheckout } from "../../components/Checkout/CheckoutPayment";
 import { Ordering } from "../../components/Checkout/OrderingProducts";
-import { CheckOut } from "../../components/Checkout/Checkout";
-
 export const Home = () => {
     return (
         <div className="px-[20px] overflow-x-hidden pt-[70px] pb-[30px] ">
@@ -16,7 +14,7 @@ export const Home = () => {
         <WhyChooseUs/>
         <NewArrival/>
         <BestSeller/>
-         <CheckOut/>
+       
         </div>
     )
 }
