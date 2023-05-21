708509a182a738a9442c5baf064f0310d9e2e2f9
Desmond Nzubechukwu Abugu <nzubestdesmond@gmail.com>
2023-05-21 12:13:12 +0100

    new change

--- src/components/CartContext/CartContext.jsx
+++ src/components/CartContext/CartContext.jsx
@@ -1,68 +1,89 @@
 import React, { createContext, useReducer, useState, useEffect } from "react";
 
+// Create a context for the cart
 export const CartContext = createContext();
 
 export const CartPro = (props) => {
+  // Cart reducer function to handle cart item updates
   const cartReducer = (cartItems, action) => {
     switch (action.type) {
       case "Add_To_Cart": {
+        // Check if the product already exists in the cart
         const existingProduct = cartItems.find(
           (product) => product.id === action.payload.id
         );
 
+        // If the product exists, increase its quantity by 1
         if (existingProduct) {
           return cartItems.map((pro) =>
             pro.id === action.payload.id ? { ...pro, quantity: pro.quantity + 1 } : pro
           );
         } else {
+          // If the product doesn't exist, add it to the cart with a quantity of 1
           return [...cartItems, { ...action.payload }];
         }
       }
 
       case "Remove_From_Cart": {
+        // Remove a product from the cart based on its ID
         const updatedCartItems = cartItems.filter((product) => product.id !== action.payload.id);
         localStorage.setItem("cartItems", JSON.stringify(updatedCartItems));
         return updatedCartItems;
       }
 
       case "Update_Quantity": {
+        // Increase the quantity of a product in the cart by 1
         return cartItems.map((pro) =>
           pro.id === action.payload.id ? { ...pro, quantity: pro.quantity + 1 } : pro
         );
       }
 
       case "Reduce_Quantity": {
+        // Decrease the quantity of a product in the cart by 1, minimum quantity is 1
         return cartItems.map((item) =>
           item.id === action.payload.id
             ? { ...item, quantity: item.quantity > 1 ? item.quantity - 1 : item.quantity }
             : item
         );
       }
-
+break;
       default:
         return cartItems;
     }
   };
 
+  // Wishlist reducer function to handle wishlist updates
   const wishListReducer = (wishList, action) => {
     switch (action.type) {
       case "Add_To_WishList":
+        // Add a product to the wishlist
         return [...wishList, action.favObj];
       case "Remove_Wish":
+        // Remove a product from the wishlist
         return wishList.filter((removeIt) => removeIt.id !== action.favObj.id);
       default:
         return wishList;
     }
   };
 
+  // Initialize cartItems state with values from localStorage or an empty array
   const [cartItems, dispatch] = useReducer(cartReducer, JSON.parse(localStorage.getItem("cartItems")) || []);
+
+  // Initialize wishList state with values from localStorage or an empty array
   const [wishList, dispatchWishList] = useReducer(wishListReducer, JSON.parse(localStorage.getItem('wishList')) || []);
+
+  // State for displaying the cart and wishlist
   const [showCart, setShowCart] = useState(false);
   const [showWish, setShowWishList] = useState(false);
-  const [fullDetail, setFullDetail] = useState([]);
+
+  // State for storing full product details and their display status
+  const [fullDetail, setFullDetail] = useState(JSON.parse(localStorage.getItem('fullDetail')) || []);
   const [details, setDetails] = useState("top-[-3000px]");
+
+  // State for storing search input and its display status
   const [Search, setSearch] = useState("top-[-3000px]");
 
+  // State for storing form inputs related to checkout process
   const [formInputs, setFormInputs] = useState({
     firstName: "",
     lastName: "",
@@ -96,27 +117,32 @@ export const CartPro = (props) => {
     paymentLink: "/checkoutAddress",
   });
 
+  // Update localStorage when cartItems state changes
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
 
+
+  ///Update localstorage product page when a particular product is clicked
+  useEffect(() => {
+    localStorage.setItem('fullDetail', JSON.stringify(fullDetail));
+  }, [fullDetail]);
+
+
+  // Clean up localStorage when the component unmounts
   useEffect(() => {
     return () => {
       localStorage.removeItem("cartItems");
     };
   }, []);
 
-useEffect(() => {
-  return () => {
-    localStorage.removeItem('wishList')
-  }
-}, [])
-
-useEffect(() => {
+  // Update localStorage when wishList state changes
+  useEffect(() => {
     localStorage.setItem('wishList', JSON.stringify(wishList));
-}, [wishList]);
+  }, [wishList]);
 
   const addToCart = (items) => {
+    // Add a product to the cart
     dispatch({
       type: "Add_To_Cart",
       payload: { ...items, quantity: 1 },
@@ -124,6 +150,7 @@ useEffect(() => {
   };
 
   const increaseCart = (product) => {
+    // Increase the quantity of a product in the cart
     dispatch({
       type: "Update_Quantity",
       payload: product,
@@ -131,6 +158,7 @@ useEffect(() => {
   };
 
   const reduceQuantity = (product) => {
+    // Decrease the quantity of a product in the cart
     dispatch({
       type: "Reduce_Quantity",
       payload: product,
@@ -138,6 +166,7 @@ useEffect(() => {
   };
 
   const removeFromCart = (product) => {
+    // Remove a product from the cart
     dispatch({
       type: "Remove_From_Cart",
       payload: product,
@@ -145,6 +174,7 @@ useEffect(() => {
   };
 
   const addToWishList = (items) => {
+    // Add a product to the wishlist
     dispatchWishList({
       type: "Add_To_WishList",
       favObj: items,
@@ -152,6 +182,7 @@ useEffect(() => {
   };
 
   const removeFromWishList = (items) => {
+    // Remove a product from the wishlist
     dispatchWishList({
       type: "Remove_Wish",
       favObj: items,
@@ -159,38 +190,47 @@ useEffect(() => {
   };
 
   const showCartItems = () => {
+    // Show cart items
     setShowCart(true);
   };
 
   const showWishList = () => {
+    // Show wishlist
     setShowWishList(true);
   };
 
   const hideWishList = () => {
+    // Hide wishlist
     setShowWishList(false);
   };
 
   const hideCartItems = () => {
+    // Hide cart items
     setShowCart(false);
   };
 
   const showFullDetail = (items) => {
+    // Show full detail of a product
     setFullDetail([items]);
   };
 
   const HideDetails = () => {
+    // Hide product details
     setDetails("top-[-3000px]");
   };
 
   const showSearch = (items) => {
+    // Show search
     setSearch("top-0");
   };
 
   const HideSearch = () => {
+    // Hide search
     setSearch("top-[-3000px]");
   };
 
   const emailProceed = () => {
+    // Proceed with email step
     setFormInputs({
       ...formInputs,
       email: "",
@@ -198,6 +238,7 @@ useEffect(() => {
   };
 
   const PaymentProceed = () => {
+    // Proceed with payment step
     setFormInputs({
       firstName: "",
       lastName: "",
@@ -212,6 +253,7 @@ useEffect(() => {
   };
 
   return (
+    // Provide the cart context to the children components
     <CartContext.Provider
       value={{
         HideSearch,
